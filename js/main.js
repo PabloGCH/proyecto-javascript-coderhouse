281fa6b6e7bd9cded726755ba99b41c4e28ff889
PabloGCH <pablochoconi@gmail.com>
2022-06-15 15:27:30 -0300

    Ingreso de productos en el DOM

--- js/main.js
+++ js/main.js
@@ -1,72 +1,67 @@
 class Product {
 	name;
 	price;
+	element;
 	constructor(name, price) {
 		this.name = name;
 		this.price = price;
+		this.element = document.createElement("div");
+		this.element.innerHTML =
+		`
+		<div class="card-body">
+			<h5 class="card-title">${name}</h1>
+			<h6 class="card-subtitle">Precio: $${price}</div>
+		</div>
+		`;
+		this.element.className = "card product-card";
 	}
 }
+
+//Necesita que haya un elemento html con el id product-container
 class ProductList {
+	container;
 	list = [];
 	constructor() {
+		this.container = document.getElementById("product-container");
 	}
+	//Recibe nombre y precio, crea un producto, agrega html a content
 	addProduct(name, price) {
-		this.list.push(new Product(name, price));
+		const product = new Product(name, price);
+		this.list.push(product);
+		this.container.append(product.element);
 	}
+	//Recibe un nombre y elimina el producto
 	removeProduct(productName) {
 		let productIndex = this.list.findIndex((item) => {
 			return item.name == productName;
 		});
 		if(productIndex == -1) {
-			alert("Ese producto no existe");
+			console.log("Se intento eliminar un elemento inexistente");
 		} else {
 			this.list.splice(productIndex, 1);
 			alert(productName + " fue eliminado");
 		}
 	}
-	alertProducts() {
-		let alertText = "";
-		this.list.forEach((item) => {
-			alertText = alertText.concat(`\n- ${item.name}`);
-		});
-		alert(alertText);
+	
+}
+const productList = new ProductList;
+
+
+function addProduct() {
+	let name = prompt("Ingrese el nombre");
+	let price = prompt("Ingrese el precio del producto");
+	if(isNaN(price)) {
+		price = 0;
+	} else {
+		price = parseInt(price);
 	}
-	alertProductsByPrice(price) {
-		let products = this.list.filter((item) => {
-			return item.price < price;
-		});
-		let alertText = "";
-		products.forEach((item) => {
-			alertText = alertText.concat(`\n- ${item.name}`);
-		});
-		alert(alertText);
-	}
+	productList.addProduct(name, price);
 }
 
-const list = new ProductList;
-let finished = false;
-while(!finished) {
-	switch(prompt("Ingrese una opcion: \n1 - Añadir producto\n2 - Eliminar producto\n3 - Mostrar productos\n4 - Mostrar productos por precio\n5 - Salir")) {
-		case "1":
-			list.addProduct(
-				prompt("Ingrese el nombre del producto: "),
-				parseInt(prompt("Ingrese el precio: "))
-			);
-			break;
-		case "2":
-			list.removeProduct(prompt("Ingrese el nombre del producto"));
-			break;
-		case "3":
-			list.alertProducts();
-			break;
-		case "4":
-			list.alertProductsByPrice(parseInt(prompt("Cual es su precio maximo?")));
-			break;
-		case "5":
-			finished = true;
-			break;
-		default:
-			alert("Ingrese una opcion valida");
-			break;
-	}
+function removeProduct() {
+	let name = prompt("Ingrese el nombre del producto");
+	productList.removeProduct(name);
 }
+
+
+
